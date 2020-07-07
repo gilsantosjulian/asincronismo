2f02f086d35b694820e5a045e25df9b3af329ba7
Julián Andrés Gil Santos <gilsantosjulian@gmail.com>
2020-07-06 20:23:00 -0500

    feat(callback/index): add callback example

---
+++ src/callback/index.js
@@ -0,0 +1,23 @@
+const sum = (num1, num2) => {
+  return num1 + num2
+}
+
+const calc = (num1, num2, callback) => {
+  return callback(num1, num2)
+}
+
+console.log(calc(1, 3, sum))
+
+const date = (callback) => {
+  console.log(new Date)
+  setTimeout(() => {
+    let date = new Date
+    callback(date)
+  }, 3000)
+}
+
+const printDate = (dateNow) => {
+  console.log(dateNow)
+}
+
+date(printDate)
